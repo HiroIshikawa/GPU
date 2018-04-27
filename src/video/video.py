11721ef2f86b397f3a05b4d48fb1f22eb94d9072
Takahiro Ishikawa <hiro52ishikawa@gmail.com>
2018-04-26 21:32:30 -0700

    Clean webcam video stream

--- src/video/video.py
+++ src/video/video.py
@@ -1,44 +1,28 @@
-# import the necessary packages
 from threading import Thread
 import cv2
 
 
 class WebcamVideoStream:
-		def __init__(self, src=0, win_w=400, win_h=300):
-				# initialize the video camera stream and read the first frame
-				# from the stream
-				self.stream = cv2.VideoCapture(src)
-				self.stream.set(3,win_w)
-				self.stream.set(4,win_h)
-				(self.grabbed, self.frame) = self.stream.read()
+	"""Threaded webcam video stream"""
+	def __init__(self, src=0, win_w=400, win_h=300):
+		self.stream = cv2.VideoCapture(src)
+		self.stream.set(3,win_w)
+		self.stream.set(4,win_h)
+		(self.grabbed, self.frame) = self.stream.read()
+		self.stopped = False
 
-				# initialize the variable used to indicate if the thread should
-				# be stopped
-				self.stopped = False
+	def start(self):
+		Thread(target=self.update, args=()).start()
+		return self
 
+	def update(self):
+		while True:
+			if self.stopped:
+				return
+			(self.grabbed, self.frame) = self.stream.read()
 
-		def start(self):
-				# start the thread to read frames from the video stream
-				Thread(target=self.update, args=()).start()
-				return self
+	def read(self):
+		return self.frame
 
-
-		def update(self):
-				# keep looping infinitely until the thread is stopped
-				while True:
-						# if the thread indicator variable is set, stop the thread
-						if self.stopped:
-							return
-
-						# otherwise, read the next frame from the stream
-						(self.grabbed, self.frame) = self.stream.read()
-
-
-		def read(self):
-				# return the frame most recently read
-				return self.frame
-
-
-		def stop(self):
-				# indicate that the thread should be stopped
-				self.stopped = True
+	def stop(self):
+		self.stopped = True
